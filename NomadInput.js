e20853e77fb119e0dfdd1d17989806343bef5e2e
NomadDsgns <90428134+NomadDsgns@users.noreply.github.com>
2021-09-09 18:57:55 -0500

    created component, README

--- NomadInput.js
+++ NomadInput.js
@@ -1,1 +1,31 @@
+import React from 'react';
+import { View, Text, Input, Stylesheet } from 'react-native';
 
+
+export default function NomadInput({ label, placeholder, value, onChange }) {
+    return (<View style={styles.container}>
+                <Text style={styles.label}>{label || ''}</Text>
+                <Input style={styles.input} 
+                        value={value}
+                        onChange={onChange} 
+                        placeholder={placeholder || ''} />
+            </View>);
+}
+
+const styles = Stylesheet.create({
+    container: {
+        flexDirection: "row",
+        width: 250,
+    },
+    label: {
+        paddingRight: 15
+    },
+    input: {
+        borderColor: "#000",
+        padding: 5,
+        borderWidth: 1,
+        borderBottomWidth: 2,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    }
+});
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# nomad-react-native-input
-Custom React Native input component
+# [Nomad Dsgns](https://www.nomaddsgns.com) Custom Input
+React Native Custom Input Component by NomadDsgns 
+
+## Instructions for Use ##
+1. Clone Component to project:
+    1. 
+2. Import into file where it's to be used in the project:
+    1.
+3. Add component into project code and pass in required/desired props:
+    - label (optional): label for the input field (ie. E-mail, Username, etc)
+    - placeholder (optional): placeholder for input field that will be displayed only when field has no text entered
+    - value (required): initial value passed to it using state 
+    - onChange (required): handler for updating state as text is entered/removed from input field
