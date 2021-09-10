afe1f4608659013ff9eec9157f7f608384a8adf5
NomadDsgns <90428134+NomadDsgns@users.noreply.github.com>
2021-09-10 02:07:42 -0500

    added validation

--- NomadInput.js
+++ NomadInput.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 
-export default function NomadInput({ label, placeholder, value, onChange }) {
+export default function NomadInput({ label, placeholder, value, onChange, validators, isPassword }) {
+    const [ error, setError ] = useState(false);
+
+    useEffect(() => {
+        setError(false)
+        if (validators && value?.length) {
+            validators.map(validator => {
+                const check = validator(value);
+                if (check) setError(check)
+            })
+        }
+    }, [ value ])
+
     return (<View style={styles.container}>
-                <Text style={styles.label}>{label || ''}</Text>
+                <Text style={styles.label}>{`${label} ↓` || ''}</Text>
                 <TextInput style={styles.input} 
                         value={value}
-                        onChange={onChange} 
-                        placeholder={placeholder || ''} />
+                        onChangeText={onChange} 
+                        placeholder={placeholder || ''}
+                        secureTextEntry={isPassword || false} />
+                <Text style={styles.error}>{error || ''}</Text>
             </View>);
 }
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: "row",
-        width: 250,
+        marginBottom: 20,
+        shadowColor: '#0043ad',
+        shadowOffset: {
+            width: 1,
+            height: 2,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
     },
     label: {
-        paddingRight: 15
+        paddingBottom: 5,
+        fontSize: 15,
+        fontWeight: "bold"
     },
     input: {
-        borderColor: "#000",
-        padding: 5,
-        borderWidth: 1,
-        borderBottomWidth: 2,
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
+        height: 40,
+        width: 300,
+        paddingHorizontal: 5,
+        backgroundColor: 'white',
+    },
+    error: {
+        paddingTop: 10,
+        color: "red",
+        textAlign: 'center',
+        position: 'relative'
     }
 });
